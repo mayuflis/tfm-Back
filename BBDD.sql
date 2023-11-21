dc9ccac58057d3bca00a1aacad6fa9a585cc4010
SergioDev <mayitavista@gmail.com>
2023-11-21 19:12:57 +0100

    Actualización de la BBDD

--- BBDD.sql
+++ BBDD.sql
@@ -15,23 +15,40 @@ CREATE SCHEMA IF NOT EXISTS `appTeacher3` DEFAULT CHARACTER SET utf8mb4 COLLATE 
 USE `appTeacher3` ;
 
 -- -----------------------------------------------------
--- Table `appTeacher3`.`Gender`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `appTeacher3`.`Gender` (
-  `idGender` INT NOT NULL AUTO_INCREMENT,
-  `gender` VARCHAR(12) NULL,
-  PRIMARY KEY (`idGender`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `appTeacher3`.`province`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `appTeacher3`.`province` (
   `idprovince` INT NOT NULL AUTO_INCREMENT,
   `name_province` VARCHAR(45) NULL,
   PRIMARY KEY (`idprovince`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `appTeacher3`.`Gender`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `appTeacher3`.`Gender` (
+  `idGender` INT NOT NULL AUTO_INCREMENT,
+  `gender` VARCHAR(12) NULL,
+  PRIMARY KEY (`idGender`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `appTeacher3`.`contacts`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `appTeacher3`.`contacts` (
+  `idcontacts` INT NOT NULL AUTO_INCREMENT,
+  `mobile` VARCHAR(12) NULL,
+  `landline` VARCHAR(10) NULL,
+  PRIMARY KEY (`idcontacts`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
 
 -- -----------------------------------------------------
@@ -48,15 +65,32 @@ CREATE TABLE IF NOT EXISTS `appTeacher3`.`users` (
   `longitude` FLOAT NOT NULL,
   `birthday` DATE NOT NULL,
   `province_idprovince` INT NULL,
+  `delete_users` TINYINT NULL DEFAULT 0,
+  `Gender_idGender` INT NULL,
+  `contacts_idcontacts` INT NULL,
   PRIMARY KEY (`idusers`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
   INDEX `fk_users_province1_idx` (`province_idprovince` ASC) VISIBLE,
+  INDEX `fk_users_Gender1_idx` (`Gender_idGender` ASC) VISIBLE,
+  INDEX `fk_users_contacts1_idx` (`contacts_idcontacts` ASC) VISIBLE,
   CONSTRAINT `fk_users_province1`
     FOREIGN KEY (`province_idprovince`)
     REFERENCES `appTeacher3`.`province` (`idprovince`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_Gender1`
+    FOREIGN KEY (`Gender_idGender`)
+    REFERENCES `appTeacher3`.`Gender` (`idGender`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_contacts1`
+    FOREIGN KEY (`contacts_idcontacts`)
+    REFERENCES `appTeacher3`.`contacts` (`idcontacts`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
 
 -- -----------------------------------------------------
@@ -66,19 +100,12 @@ CREATE TABLE IF NOT EXISTS `appTeacher3`.`teachers` (
   `id_teachers` INT NOT NULL AUTO_INCREMENT,
   `validate` TINYINT(1) NULL DEFAULT 0,
   `image` VARCHAR(80) NULL,
-  `Gender_idGender` INT NULL,
   `users_idusers` INT NOT NULL,
   `description_prof` TINYTEXT NULL,
   `title_prof` VARCHAR(200) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' NULL,
   `description_class` TINYTEXT NULL,
   PRIMARY KEY (`id_teachers`),
-  INDEX `fk_teachers_Gender1_idx` (`Gender_idGender` ASC) VISIBLE,
   INDEX `fk_teachers_users1_idx` (`users_idusers` ASC) VISIBLE,
-  CONSTRAINT `fk_teachers_Gender1`
-    FOREIGN KEY (`Gender_idGender`)
-    REFERENCES `appTeacher3`.`Gender` (`idGender`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_teachers_users1`
     FOREIGN KEY (`users_idusers`)
     REFERENCES `appTeacher3`.`users` (`idusers`)
@@ -96,31 +123,6 @@ CREATE TABLE IF NOT EXISTS `appTeacher3`.`subjects` (
   `idsubjects` INT NOT NULL AUTO_INCREMENT,
   `name` ENUM('Matemáticas', 'Lenguaje', 'Inglés', 'Programación', 'Física', 'Biología', 'Química') NULL,
   PRIMARY KEY (`idsubjects`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `appTeacher3`.`students`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `appTeacher3`.`students` (
-  `idstudents` INT NOT NULL AUTO_INCREMENT,
-  `state` TINYINT(1) NULL DEFAULT 1,
-  `image` VARCHAR(80) NULL,
-  `Gender_idGender` INT NULL,
-  `users_idusers` INT NOT NULL,
-  PRIMARY KEY (`idstudents`),
-  INDEX `fk_students_Gender1_idx` (`Gender_idGender` ASC) VISIBLE,
-  INDEX `fk_students_users1_idx` (`users_idusers` ASC) VISIBLE,
-  CONSTRAINT `fk_students_Gender1`
-    FOREIGN KEY (`Gender_idGender`)
-    REFERENCES `appTeacher3`.`Gender` (`idGender`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_students_users1`
-    FOREIGN KEY (`users_idusers`)
-    REFERENCES `appTeacher3`.`users` (`idusers`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
@@ -134,97 +136,21 @@ CREATE TABLE IF NOT EXISTS `appTeacher3`.`reviews` (
   `opinions` TEXT NULL,
   `recommendations` TEXT NULL,
   `teachers_id_teachers` INT NOT NULL,
-  `students_idstudents` INT NOT NULL,
   `rate` TINYINT(1) NULL,
+  `users_idusers` INT NOT NULL,
   PRIMARY KEY (`id_reviews`),
   INDEX `fk_reviews_teachers1_idx` (`teachers_id_teachers` ASC) VISIBLE,
-  INDEX `fk_reviews_students1_idx` (`students_idstudents` ASC) VISIBLE,
+  INDEX `fk_reviews_users1_idx` (`users_idusers` ASC) VISIBLE,
   CONSTRAINT `fk_reviews_teachers1`
     FOREIGN KEY (`teachers_id_teachers`)
     REFERENCES `appTeacher3`.`teachers` (`id_teachers`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_reviews_students1`
-    FOREIGN KEY (`students_idstudents`)
-    REFERENCES `appTeacher3`.`students` (`idstudents`)
+  CONSTRAINT `fk_reviews_users1`
+    FOREIGN KEY (`users_idusers`)
+    REFERENCES `appTeacher3`.`users` (`idusers`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `appTeacher3`.`contactsTeachers`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `appTeacher3`.`contactsTeachers` (
-  `idcontacts` INT NOT NULL AUTO_INCREMENT,
-  `mobile` VARCHAR(12) NULL,
-  `landline` VARCHAR(10) NULL,
-  `teachers_id_teachers` INT NOT NULL,
-  PRIMARY KEY (`idcontacts`),
-  INDEX `fk_contactsTeachers_teachers1_idx` (`teachers_id_teachers` ASC) VISIBLE,
-  CONSTRAINT `fk_contactsTeachers_teachers1`
-    FOREIGN KEY (`teachers_id_teachers`)
-    REFERENCES `appTeacher3`.`teachers` (`id_teachers`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
-
-
--- -----------------------------------------------------
--- Table `appTeacher3`.`contactsStudents`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `appTeacher3`.`contactsStudents` (
-  `idcontacts` INT NOT NULL AUTO_INCREMENT,
-  `mobile` VARCHAR(12) NULL,
-  `landline` VARCHAR(10) NULL,
-  `students_idstudents` INT NOT NULL,
-  PRIMARY KEY (`idcontacts`),
-  INDEX `fk_contactsStudents_students1_idx` (`students_idstudents` ASC) VISIBLE,
-  CONSTRAINT `fk_contactsStudents_students1`
-    FOREIGN KEY (`students_idstudents`)
-    REFERENCES `appTeacher3`.`students` (`idstudents`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `appTeacher3`.`teachers_has_students`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `appTeacher3`.`teachers_has_students` (
-  `teachers_id_teachers` INT NOT NULL,
-  `students_idstudents` INT NOT NULL,
-  INDEX `fk_teachers_has_students_students1_idx` (`students_idstudents` ASC) VISIBLE,
-  INDEX `fk_teachers_has_students_teachers1_idx` (`teachers_id_teachers` ASC) VISIBLE,
-  CONSTRAINT `fk_teachers_has_students_teachers1`
-    FOREIGN KEY (`teachers_id_teachers`)
-    REFERENCES `appTeacher3`.`teachers` (`id_teachers`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_teachers_has_students_students1`
-    FOREIGN KEY (`students_idstudents`)
-    REFERENCES `appTeacher3`.`students` (`idstudents`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
-
-
--- -----------------------------------------------------
--- Table `appTeacher3`.`admin`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `appTeacher3`.`admin` (
-  `idadmin` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(20) NULL,
-  `lastname` VARCHAR(45) NULL,
-  `email` VARCHAR(60) NULL,
-  `password` VARCHAR(30) NULL,
-  `image` VARCHAR(80) NULL,
-  `role` VARCHAR(7) NULL DEFAULT 'admin',
-  PRIMARY KEY (`idadmin`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
@@ -249,6 +175,29 @@ CREATE TABLE IF NOT EXISTS `appTeacher3`.`teachers_has_subjects` (
   CONSTRAINT `fk_teachers_has_subjects_subjects1`
     FOREIGN KEY (`subjects_idsubjects`)
     REFERENCES `appTeacher3`.`subjects` (`idsubjects`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+
+-- -----------------------------------------------------
+-- Table `appTeacher3`.`teachers_has_users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `appTeacher3`.`teachers_has_users` (
+  `teachers_id_teachers` INT NOT NULL,
+  `users_idusers` INT NOT NULL,
+  INDEX `fk_teachers_has_users_users1_idx` (`users_idusers` ASC) VISIBLE,
+  INDEX `fk_teachers_has_users_teachers1_idx` (`teachers_id_teachers` ASC) VISIBLE,
+  CONSTRAINT `fk_teachers_has_users_teachers1`
+    FOREIGN KEY (`teachers_id_teachers`)
+    REFERENCES `appTeacher3`.`teachers` (`id_teachers`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_teachers_has_users_users1`
+    FOREIGN KEY (`users_idusers`)
+    REFERENCES `appTeacher3`.`users` (`idusers`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
