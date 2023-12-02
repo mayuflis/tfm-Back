0ecc40740939bf82a590f37113560c0ac6da93d4
pauubach <p.ubach1999@gmail.com>
2023-12-02 23:48:39 +0100

    router.use teachers added

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const middlewaresAuth = require("../middlewares/auth.middleware");
 const { checkRoleTeachers } = require("../middlewares/role.middleware");
-router.use("/users", require("./api/users"));
 
+router.use("/users", require("./api/users"));
 router.use("/webPublic", require("./api/webPublic"));
+router.use('/teachers', require('./api/teachers'));
 
 module.exports = router;
