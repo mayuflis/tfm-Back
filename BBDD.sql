b57cb8e3fc4128d40d9e23589653e29ea07f02d3
SergioDev <mayitavista@gmail.com>
2023-12-04 03:45:15 +0100

    Actualización BBDD

--- BBDD.sql
+++ BBDD.sql
@@ -18,6 +18,31 @@ USE `appteacher3`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `class`
+--
+
+DROP TABLE IF EXISTS `class`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `class` (
+  `teachers_id_teachers` int NOT NULL,
+  `users_idusers` int NOT NULL,
+  `idsubject` int NOT NULL,
+  `class date` date NOT NULL,
+  `start_time` time DEFAULT NULL,
+  `start_end` time DEFAULT NULL,
+  `validate` tinyint DEFAULT '0',
+  `level` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `date_contact` datetime DEFAULT NULL,
+  `acceptance_date` datetime DEFAULT NULL,
+  KEY `fk_teachers_has_users_users1_idx` (`users_idusers`),
+  KEY `fk_teachers_has_users_teachers1_idx` (`teachers_id_teachers`),
+  CONSTRAINT `fk_teachers_has_users_teachers1` FOREIGN KEY (`teachers_id_teachers`) REFERENCES `teachers` (`id_teachers`),
+  CONSTRAINT `fk_teachers_has_users_users1` FOREIGN KEY (`users_idusers`) REFERENCES `users` (`idusers`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `contacts`
 --
 
@@ -29,17 +54,8 @@ CREATE TABLE `contacts` (
   `mobile` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `landline` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`idcontacts`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `contacts`
---
-
-LOCK TABLES `contacts` WRITE;
-/*!40000 ALTER TABLE `contacts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `contacts` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `gender`
@@ -52,18 +68,8 @@ CREATE TABLE `gender` (
   `idGender` int NOT NULL AUTO_INCREMENT,
   `gender` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`idGender`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `gender`
---
-
-LOCK TABLES `gender` WRITE;
-/*!40000 ALTER TABLE `gender` DISABLE KEYS */;
-INSERT INTO `gender` VALUES (3,'masculino'),(4,'femenino');
-/*!40000 ALTER TABLE `gender` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `province`
@@ -77,18 +83,8 @@ CREATE TABLE `province` (
   `name_province` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`idprovince`),
   UNIQUE KEY `name_province_UNIQUE` (`name_province`)
-) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `province`
---
-
-LOCK TABLES `province` WRITE;
-/*!40000 ALTER TABLE `province` DISABLE KEYS */;
-INSERT INTO `province` VALUES (88,'A coruña'),(95,'Álava'),(74,'Albacete'),(83,'Alicante'),(50,'Almería'),(61,'Asturias'),(66,'Ávila'),(86,'Badajoz'),(79,'Barcelona'),(96,'Bizkaia'),(67,'Burgos'),(87,'Cáceres'),(51,'Cádiz'),(65,'Cantabria'),(85,'Castellón'),(75,'Ciudad Real'),(52,'Córdoba'),(76,'Cuenca'),(80,'Girona'),(53,'Granada'),(77,'Guadalajara'),(97,'Guipúzcoa'),(54,'Huelva'),(58,'Huesca'),(62,'Illes Balears'),(55,'Jaen'),(98,'La Rioja'),(63,'Las Palmas'),(68,'León'),(81,'Lleida'),(89,'Lugo'),(92,'Madrid'),(56,'Málaga'),(93,'Murcia'),(94,'Navarra'),(90,'Ourense'),(69,'Palencia'),(91,'Pontevedra'),(70,'Salamanca'),(64,'Santa Cruz de Tenerife'),(71,'Segovia'),(57,'Sevilla'),(82,'Tarragona'),(59,'Terual'),(78,'Toledo'),(84,'Valencia'),(72,'Valladolid'),(73,'Zamora'),(60,'Zaragoza');
-/*!40000 ALTER TABLE `province` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `reviews`
@@ -111,17 +107,8 @@ CREATE TABLE `reviews` (
   KEY `fk_reviews_users1_idx` (`users_idusers`),
   CONSTRAINT `fk_reviews_teachers1` FOREIGN KEY (`teachers_id_teachers`) REFERENCES `teachers` (`id_teachers`),
   CONSTRAINT `fk_reviews_users1` FOREIGN KEY (`users_idusers`) REFERENCES `users` (`idusers`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `reviews`
---
-
-LOCK TABLES `reviews` WRITE;
-/*!40000 ALTER TABLE `reviews` DISABLE KEYS */;
-/*!40000 ALTER TABLE `reviews` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `subjects`
@@ -134,18 +121,8 @@ CREATE TABLE `subjects` (
   `idsubjects` int NOT NULL AUTO_INCREMENT,
   `name` enum('Matemáticas','Lenguaje','Inglés','Programación','Física','Biología','Química') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`idsubjects`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `subjects`
---
-
-LOCK TABLES `subjects` WRITE;
-/*!40000 ALTER TABLE `subjects` DISABLE KEYS */;
-INSERT INTO `subjects` VALUES (8,'Matemáticas'),(9,'Lenguaje'),(10,'Inglés'),(11,'Programación'),(12,'Física'),(13,'Biología'),(14,'Química');
-/*!40000 ALTER TABLE `subjects` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `teachers`
@@ -157,27 +134,19 @@ DROP TABLE IF EXISTS `teachers`;
 CREATE TABLE `teachers` (
   `id_teachers` int NOT NULL AUTO_INCREMENT,
   `validate` tinyint(1) DEFAULT '0',
-  `image` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `users_idusers` int NOT NULL,
   `description_prof` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `title_prof` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `description_class` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `experience` tinyint DEFAULT NULL,
+  `validation_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id_teachers`),
   KEY `fk_teachers_users1_idx` (`users_idusers`),
   CONSTRAINT `fk_teachers_users1` FOREIGN KEY (`users_idusers`) REFERENCES `users` (`idusers`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `teachers`
---
-
-LOCK TABLES `teachers` WRITE;
-/*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
-/*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `teachers_has_subjects`
@@ -198,41 +167,6 @@ CREATE TABLE `teachers_has_subjects` (
   CONSTRAINT `fk_teachers_has_subjects_teachers1` FOREIGN KEY (`teachers_id_teachers`) REFERENCES `teachers` (`id_teachers`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `teachers_has_subjects`
---
-
-LOCK TABLES `teachers_has_subjects` WRITE;
-/*!40000 ALTER TABLE `teachers_has_subjects` DISABLE KEYS */;
-/*!40000 ALTER TABLE `teachers_has_subjects` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `teachers_has_users`
---
-
-DROP TABLE IF EXISTS `teachers_has_users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `teachers_has_users` (
-  `teachers_id_teachers` int NOT NULL,
-  `users_idusers` int NOT NULL,
-  KEY `fk_teachers_has_users_users1_idx` (`users_idusers`),
-  KEY `fk_teachers_has_users_teachers1_idx` (`teachers_id_teachers`),
-  CONSTRAINT `fk_teachers_has_users_teachers1` FOREIGN KEY (`teachers_id_teachers`) REFERENCES `teachers` (`id_teachers`),
-  CONSTRAINT `fk_teachers_has_users_users1` FOREIGN KEY (`users_idusers`) REFERENCES `users` (`idusers`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `teachers_has_users`
---
-
-LOCK TABLES `teachers_has_users` WRITE;
-/*!40000 ALTER TABLE `teachers_has_users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `teachers_has_users` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -257,6 +191,9 @@ CREATE TABLE `users` (
   `contacts_idcontacts` int DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `image` varchar(80) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `registration_date` datetime DEFAULT NULL,
+  `date_request _withdrawal` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`idusers`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   KEY `fk_users_province1_idx` (`province_idprovince`),
@@ -265,17 +202,8 @@ CREATE TABLE `users` (
   CONSTRAINT `fk_users_contacts1` FOREIGN KEY (`contacts_idcontacts`) REFERENCES `contacts` (`idcontacts`),
   CONSTRAINT `fk_users_Gender1` FOREIGN KEY (`Gender_idGender`) REFERENCES `gender` (`idGender`),
   CONSTRAINT `fk_users_province1` FOREIGN KEY (`province_idprovince`) REFERENCES `province` (`idprovince`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -286,7 +214,7 @@ UNLOCK TABLES;
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 /*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `insertUsersToRole` AFTER INSERT ON `users` FOR EACH ROW begin
-if new.role='teachers' then
+if new.role='Profesor' then
 	 insert into teachers(users_idusers)values(new.idusers);
  
 end if;
@@ -297,6 +225,10 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Dumping events for database 'appteacher3'
+--
 
 --
 -- Dumping routines for database 'appteacher3'
@@ -351,4 +283,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-26 10:00:29
+-- Dump completed on 2023-12-03 22:16:57
